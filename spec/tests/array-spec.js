941335d3e7fa937a75febe0378323faa3859a4a8
Lachlan Hunt <lhunt@atlassian.com>
2016-06-21 15:50:56 +1000

    Ensure sorting the slice only sorts within the range

--- spec/tests/array-spec.js
+++ spec/tests/array-spec.js
@@ -2,8 +2,8 @@ describe("Array methods", function() {
 	let ArraySlice = require("../../lib/ArraySlice");
 
 	it("should sort the subset of the array", function() {
-		let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-		let b = [0, 1, 2, 7, 3, 6, 4, 5, 8, 9];
+		let a = [8, 9, 6, 0, 1, 2, 5, 7, 4, 3];
+		let b = [8, 9, 6, 1, 5, 7, 0, 2, 4, 3];
 		let c = ArraySlice(b, 3, 8);
 
 		function compareNumbers(a, b) {
