c500b0658b72b74dfeb1c88a02a622211aafe7fe
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2016-06-22 23:08:48 +1000

    Test generic Array prototype methods on slices

--- spec/tests/array-spec.js
+++ spec/tests/array-spec.js
@@ -1,7 +1,229 @@
-describe("Array methods", function() {
+describe("Array prototype method", function() {
 	let ArraySlice = require("../../lib/ArraySlice");
 
-	it("should sort the subset of the array", function() {
+	it(".concat() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,0,0,0,1,2,3,4,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		let d = c.concat([5,6,7,8,9]);
+
+		expect(d).toEqual(a);
+	});
+
+	it(".copyWithin() should not copy past the end of the slice", function() {
+		let a = [0,1,2,3,4,5,3,4,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.copyWithin(3, 0);
+
+		expect(b).toEqual(a);
+	});
+
+	it(".every() should invoke the callback only for elements within the slice", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = ArraySlice(a, 3, 8);
+
+		function callback(value, index, array) {
+			return index === value - 3 && value >=0 && value < 8 && array === b;
+		}
+
+		expect(b.every(callback)).toBe(true);
+	});
+
+	it(".fill() should set values only within the slice", function() {
+		let a = [0,1,2,0,0,0,0,0,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.fill(0);
+
+		expect(b).toEqual(a);
+	});
+
+/*
+	it(".filter() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.filter();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".find() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.find();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".findIndex() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.findIndex();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".forEach() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.forEach();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".includes() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.includes();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".indexOf() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.indexOf();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".join() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.join();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".keys() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.keys();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".lastIndexOf() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.lastIndexOf();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".map() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.map();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".pop() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.pop();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".push() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.push();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".reduce() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.reduce();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".reduceRight() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.reduceRight();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".reverse() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.reverse();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".shift() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.shift();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".slice() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.slice();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".some() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.some();
+
+		expect(b).toEqual(a);
+	});
+*/
+	it(".sort() should sort the subset of the array", function() {
 		let a = [8, 9, 6, 0, 1, 2, 5, 7, 4, 3];
 		let b = [8, 9, 6, 1, 5, 7, 0, 2, 4, 3];
 		let c = ArraySlice(b, 3, 8);
@@ -14,4 +236,85 @@ describe("Array methods", function() {
 
 		expect(b).toEqual(a);
 	});
+/*
+	it(".splice() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.splice();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".toLocaleString() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.toLocaleString();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".toSource() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.toSource();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".toString() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.toString();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".unshift() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.unshift();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".entries() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.entries();
+
+		expect(b).toEqual(a);
+	});
+
+	it(".values() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c.values();
+
+		expect(b).toEqual(a);
+	});
+
+	it("[@@iterator]() should ", function() {
+		let a = [0,1,2,3,4,5,6,7,8,9];
+		let b = [0,1,2,3,4,5,6,7,8,9];
+		let c = ArraySlice(b, 3, 8);
+
+		c[Symbol.iterator]();
+
+		expect(b).toEqual(a);
+	});
+*/
 });
